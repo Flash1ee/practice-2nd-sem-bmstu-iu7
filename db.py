dc7fcc1ed46303dbb04d22149c486e418d78b675
Ivakhnenko Dmitriy <ivahnencko01@gmail.com>
2020-05-10 22:04:43 +0300

    URL of DB was encapsulated

--- db.py
+++ db.py
@@ -2,21 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pymysql
-
+import json
 import sys
+
 sys.path.append("./models/")
-
 from DataBaseClasses import Role, BlockedTicket, Token, Message, User, Ticket, Base
-# from BlockedTicket import BlockedTicket
-# from Token import Token
-# from Message import Message
-# from User import User
-# from Ticket import Ticket
-
 
 
 pymysql.install_as_MySQLdb()
-engine = create_engine('mysql://admin_4plus1:02xucJKMyT@larin.tech:3306/admin_4plus1', echo=True)
+
+config = json.load(open(".\config.json"))
+engine = create_engine(config['database']['url'], echo=True)
 Session = sessionmaker(bind=engine)
 
 
@@ -26,7 +22,7 @@ role_сlient = Role(name='Клиент')
 
 manager_vladimir = User(name='Vladimir_manager', conversation=597, role_id=1)
 client_demasek = User(name='Demasek_client', conversation=100, role_id=2)
-test_ticket1 = Ticket(manager_id=manager_vladimir.id, client_id=client_demasek.id, title='Вова тоже гей! Проблема решенааааааа')
+test_ticket1 = Ticket(manager_id=manager_vladimir.id, client_id=client_demasek.id, title='Вова НЕ ГЕЙ. Проблема все еще есть!')
 
 # client_dima = User(id=46, name='Dmirty_client', conversation=111, role_id=2)
 # test_ticket = Ticket(manager_id=manager_vladimir.id, title='ДИМА ГЕЙ, НАДО РЕШАТЬ ПРОБЛЕМУ!')
