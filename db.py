2a10d96cf165538a9f2e627a0751e37fb0942cb5
Troshkin Nikolai <nicktrs@mail.ru>
2020-05-23 15:30:53 +0300

    Initial commit for branch

---
+++ db.py
@@ -0,0 +1,50 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import json
+import pymysql
+import sys
+
+from models.DataBaseClasses import Role, BlockedTicket, Token, Message, User, Ticket, Base
+
+
+pymysql.install_as_MySQLdb()
+
+config = json.load(open("./config.json"))
+engine = create_engine(config['database']['url'], echo=True)
+Session = sessionmaker(bind=engine)
+
+
+# role_manager = Role(name='Менеджер')
+# role_сlient = Role(name='Клиент')
+
+
+# manager_vladimir = User(id=1, name='Vladimir_manager', conversation=597, role_id=1)
+# client_demasek = User(id=2, name='Demasek_client', conversation=100, role_id=2)
+# test_ticket1 = Ticket(id=1, manager_id=manager_vladimir.id, client_id=client_demasek.id, title='Вова Проблема все еще есть!')
+
+# client_dima = User(id=46, name='Dmirty_client', conversation=111, role_id=2)
+# test_ticket = Ticket(manager_id=manager_vladimir.id, title= НАДО РЕШАТЬ ПРОБЛЕМУ!')
+# # test_ticket2 = Ticket(manager_id=manager_vladimir.id, client_id=client_demasek.id, title='ЭЭЭ, а где вообще клиент ид')
+# test_ticket2 = Ticket(id=12, manager_id=manager_vladimir.id, client_id=client_dima.id, title='ЭЭЭ, а где вообще клиент ид')
+
+# test_message = Message(ticket_id=test_ticket2.id, sender_id=test_ticket2.client_id, body='Дарова:^), Как делишки, парнишка?')
+# blocked = BlockedTicket(id=12, ticket_id=test_ticket2.id, manager_id=manager_vladimir.id, reason=' )
+
+
+Base.metadata.create_all(engine)
+session = Session()
+# session.add(role_manager)
+# session.add(role_сlient)
+# session.add(manager_vladimir)
+# # session.add(client_dima)
+# session.add(client_demasek)
+# # session.add(test_ticket)
+# session.add(test_ticket1)
+# # session.add(test_message)
+# session.add(blocked)
+
+# session.commit()
+
+for instance, in session.query(User.name).filter(User.name == 'Demasek_client'):
+    print('ALL NAMES:', instance)
---
+++ models/DataBaseClasses.py
@@ -0,0 +1,85 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, BigInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10))
+
+    # Relationship
+    users = relationship('User', order_by='User.role_id', back_populates='role')
+    tokens = relationship('Token', order_by='Token.role_id', back_populates='role')
+
+class User(Base):
+    __tablename__  = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    conversation = Column(BigInteger, primary_key=True)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    
+    # Relationship
+    role = relationship('Role', back_populates='users')
+    
+    ticket_manager = relationship('Ticket', order_by='Ticket.manager_id', back_populates='manager', foreign_keys='Ticket.manager_id')
+    ticket_client = relationship('Ticket', order_by='Ticket.client_id', back_populates='client', foreign_keys='Ticket.client_id')
+    message = relationship('Message', order_by='Message.sender_id', back_populates='sender')
+
+
+class Ticket(Base):
+    __tablename__ = 'tickets'
+
+    id = Column(Integer, primary_key=True)
+    manager_id = Column(Integer, ForeignKey('users.id'))
+    client_id = Column(Integer, ForeignKey('users.id'))
+    title = Column(String(50))
+    start_date = Column(DateTime)
+    close_date = Column(DateTime)
+
+    # Relationship
+    client = relationship('User', foreign_keys=[client_id], back_populates='ticket_client')
+    manager = relationship('User', foreign_keys=[manager_id], back_populates='ticket_manager')
+
+    isblocked = relationship('BlockedTicket', order_by='BlockedTicket.ticket_id', back_populates='ticket')
+    message = relationship('Message', order_by='Message.ticket_id', back_populates='ticket')
+
+
+class BlockedTicket(Base):
+    __tablename__ = 'blocked_tickets'
+
+    id = Column(Integer, primary_key=True)
+    ticket_id = Column(Integer, ForeignKey('tickets.id'))
+    manager_id = Column(Integer, ForeignKey('users.id'))
+    reason = Column(String(50))
+
+    # Relationship
+    ticket = relationship('Ticket', back_populates='isblocked')
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    ticket_id = Column(Integer, ForeignKey('tickets.id'))
+    sender_id = Column(Integer, ForeignKey('users.id'))
+    body = Column(String(500))
+    date = Column(DateTime)
+
+    # Relationship
+    ticket = relationship('Ticket', back_populates='message')
+    sender = relationship('User', back_populates='message')
+
+
+class Token(Base):
+    __tablename__ = 'tokens'
+
+    value = Column(Integer, primary_key=True)
+    expires_date = Column(DateTime)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    # Relationship
+    role = relationship('Role', back_populates='tokens')
