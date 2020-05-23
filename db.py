257da52cebe0dd08b28f9feee0b907904ac08656
Ivakhnenko Dmitriy <ivahnencko01@gmail.com>
2020-05-23 12:41:15 +0300

    Fix import

--- db.py
+++ db.py
@@ -1,28 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pymysql
 import json
+import pymysql
 import sys
 
-sys.path.append("./models/")
-from DataBaseClasses import Role, BlockedTicket, Token, Message, User, Ticket, Base
+from models.DataBaseClasses import Role, BlockedTicket, Token, Message, User, Ticket, Base
 
 
 pymysql.install_as_MySQLdb()
 
-config = json.load(open(".\config.json"))
+config = json.load(open("./config.json"))
 engine = create_engine(config['database']['url'], echo=True)
 Session = sessionmaker(bind=engine)
 
 
-role_manager = Role(name='Менеджер')
-role_сlient = Role(name='Клиент')
+# role_manager = Role(name='Менеджер')
+# role_сlient = Role(name='Клиент')
 
 
-manager_vladimir = User(name='Vladimir_manager', conversation=597, role_id=1)
-client_demasek = User(name='Demasek_client', conversation=100, role_id=2)
-test_ticket1 = Ticket(manager_id=manager_vladimir.id, client_id=client_demasek.id, title='Вова  Проблема все еще есть!')
+# manager_vladimir = User(id=1, name='Vladimir_manager', conversation=597, role_id=1)
+# client_demasek = User(id=2, name='Demasek_client', conversation=100, role_id=2)
+# test_ticket1 = Ticket(id=1, manager_id=manager_vladimir.id, client_id=client_demasek.id, title='Вова Проблема все еще есть!')
 
 # client_dima = User(id=46, name='Dmirty_client', conversation=111, role_id=2)
 # test_ticket = Ticket(manager_id=manager_vladimir.id, title= НАДО РЕШАТЬ ПРОБЛЕМУ!')
@@ -35,15 +34,17 @@ test_ticket1 = Ticket(manager_id=manager_vladimir.id, client_id=client_demasek.i
 
 Base.metadata.create_all(engine)
 session = Session()
-session.add(role_manager)
-session.add(role_сlient)
-session.add(manager_vladimir)
-# session.add(client_dima)
-session.add(client_demasek)
-# session.add(test_ticket)
-session.add(test_ticket1)
-# session.add(test_message)
+# session.add(role_manager)
+# session.add(role_сlient)
+# session.add(manager_vladimir)
+# # session.add(client_dima)
+# session.add(client_demasek)
+# # session.add(test_ticket)
+# session.add(test_ticket1)
+# # session.add(test_message)
 # session.add(blocked)
 
-session.commit()
+# session.commit()
 
+for instance, in session.query(User.name).filter(User.name == 'Demasek_client'):
+    print('ALL NAMES:', instance)
