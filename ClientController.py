b18109a7fae6bbe9f03a1fab7819922e0444c70c
Volodya Larin <denixed@ya.ru>
2020-06-07 17:47:56 +0300

    some transfer

--- ClientController.py
+++ ClientController.py
@@ -5,37 +5,4 @@
 from models.DataBaseClasses import *
 
 def init(bot):
-    @bot.message_handler(commands = ["ticket_add"])
-    def create_ticket(message):
-        """
-            Открытие нового тикета
-        """
-        user = message.user
-        if not user:
-            bot.send_message(message.chat.id, "Для того, чтобы создать тикет, необходимо зарегистрироваться в " \
-                            "системе. Воспользуйтесь командой /start.")
-        else:
-            if user.role_id != RoleNames.CLIENT.value:
-                bot.send_message(message.chat.id, "Комманда /ticket_add доступна только для клиентов.")
-            else:    
-                bot.send_message(message.chat.id, user.name + ", для начала кратко сформулируйте Вашу проблему:")
-                bot.register_next_step_handler(message, get_title)
-    def get_title(message):
-        """
-            Получение заголовка тикета
-        """
-        user = message.user
-        new_ticket = Ticket.create(message.session, message.text, message.chat.id)
-        if not new_ticket:
-            bot.send_message(message.chat.id, user.name + ", извините, в системе нет ни одного менеджера. Пожалуйста, обратитесь спустя пару минут.")
-            return
-        bot.send_message(message.chat.id, "Отлично. Теперь опишите Ваш вопрос более детально: ")
-        bot.register_next_step_handler(message, get_ticket_body, new_ticket.id)
-    def get_ticket_body(message, ticket_id: int):
-        """
-            Получение описания тикета
-        """
-        Message.add(message.session, message.text, ticket_id, message.chat.id)
-        bot.send_message(message.chat.id, "Ваш вопрос успешно отправлен. В ближайшем времени с Вами свяжется менеджер.")
-
-    
+    pass;    
